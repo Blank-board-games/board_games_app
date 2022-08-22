df04c8187f773dd674755d79a8c6f3a580031369
Ilze Rigerte <irigerte@gmail.com>
2022-08-22 15:48:11 +0300

    collection part

---
+++ resources/views/collection.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+  <link href="{{asset('css/collection.css')}}" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+ 
+  <h1>Tote Bags</h1>
+
+  <div class="filter-wrapper">
+    <div class="top-filter">
+  <img src="{{asset('icons/filter.svg')}}" alt="">
+  <p>Filter and sort</p>
+  </div>
+  <div>
+  <p>6 products</p>
+  </div>
+  </div>
+  <div class="line one"></div>
+  <div class="links">
+   
+    <div>
+    <h1>Quick links</h1>
+    <div class="wrap">
+<p>Bags</p>
+<p>Shoes</p>
+<p>Lookbook</p>
+</div>
+</div>
+<div>
+<h1>Info</h1>
+<div class="wrap">
+<p>About</p>
+<p>Contact us</p>
+<p>Shipping policy</p>
+<p>Blog</p>
+</div>
+</div>
+<div>
+<h1>Our mission</h1>
+<div class="wrap">
+<p>Quality materials, good designs, craftsmanship and sustainability.</p>
+
+</div>
+</div>
+<div class="sub-wrap">
+<h1 class="subscription">Subscribe to our emails</h1>
+<input type="text" placeholder="Email">
+<div class="icon-wrap">
+  <img src="{{asset('icons/twit.svg')}}" alt="">
+  <img src="{{asset('icons/facebook.svg')}}" alt="">
+  <img src="{{asset('icons/instagram.svg')}}" alt="">
+  <img src="{{asset('icons/tiktok.svg')}}" alt="">
+  <img src="{{asset('icons/youtube.svg')}}" alt="">
+</div>
+</div>
+
+  </div>
+  <div class="line two"></div>
+</body>
+</html>
