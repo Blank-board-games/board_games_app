a16c9275eb79d870615e0b7005d80be79427430a
Annija <annijakaritone@gmail.com>
2022-08-24 16:27:10 +0300

    Added cart design

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+      $products = Product::limit(8)->offset(0)
+        ->orderBy('updated_at', 'DESC')
+        ->where('count_in_stock', '<>', 0)
+        ->get();
+      return view('cart')->with('products', $products);
+    }
+}
