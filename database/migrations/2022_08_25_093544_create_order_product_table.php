93be15511b3911342108fa4f26cb42f449528b58
Annija <annijakaritone@gmail.com>
2022-08-25 14:10:16 +0300

    Added order and order-product table

---
+++ database/migrations/2022_08_25_093544_create_order_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id')->constrained()->onDelete('cascade');
+            $table->integer('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->float('product_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+};
